7fae057031d45864d1d1986fbd0403bad29ece08
Lucho <lucho.lsc46@gmail.com>
2025-05-24 23:38:24 -0300

    fix: styles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var swiper = Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     centeredSlides: true,
     loop: true,
     spaceBetween: 30,
     grabCursor: true,
     navigation: {
-        nextEl: '.swiper-butoon-next',
-        prevEl: '.swiper-butoon-prev'
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
     },
     breakpoints: {
         991: {
